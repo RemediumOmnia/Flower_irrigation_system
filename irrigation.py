5835688ad22327199596f2ea3dcd5be48763ac1e
RemediumOmnia <147998373+RemediumOmnia@users.noreply.github.com>
2023-11-04 00:01:43 +0100

    Update irrigation.py LED

--- irrigation.py
+++ irrigation.py
@@ -1,2 +1,14 @@
-print("hello")
-print("test")
+from gpiozero import LED
+from time import sleep
+
+print("script start")
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
+
+print("script stop")
